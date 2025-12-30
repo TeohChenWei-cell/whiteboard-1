0b5d99e73271722628dca6bc44149f041f5c8d82
TeohChenWei-cell <teohchenweiwork@gmail.com>
2025-12-31 01:40:40 +0800

    Create p6.py

---
+++ src/p6.py
@@ -0,0 +1,21 @@
+def max_occurring_char(s):
+    counts = {}
+    max_char = None
+    max_count = 0
+
+    for ch in s:
+        if ('a' <= ch <= 'z') or ('A' <= ch <= 'Z') or ('0' <= ch <= '9'):
+            if ch in counts:
+                counts[ch] += 1
+            else:
+                counts[ch] = 1
+
+            if counts[ch] > max_count:
+                max_count = counts[ch]
+                max_char = ch
+
+    return max_char, max_count
+
+text = "testing." // change this to any word
+char, count = max_occurring_char(text)
+print(f"Character: '{char}', Occurrence: {count}")
