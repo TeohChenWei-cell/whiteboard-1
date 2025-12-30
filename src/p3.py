f3ae2fe787f13c4d16fa3b2ed8c80a0ae50f9b18
TeohChenWei-cell <teohchenweiwork@gmail.com>
2025-12-31 01:05:21 +0800

    Create p3.py

---
+++ src/p3.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+def generate_fibonacci(count):
+    for i in range(count):
+        print(fibonacci(i), end=", ")
+
+generate_fibonacci(100) //type how many you fibonacci you wanted to generate
